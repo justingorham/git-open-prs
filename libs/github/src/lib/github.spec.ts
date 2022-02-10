4f0a46ef2d3a695caec1f013d0e3923448dc416d
Justin Gorham <justingorham@gmail.com>
2022-02-09 20:39:20 -0500

    Working on unit tests - github rate limited me 🤦‍♂️ - if axios-mock-adapter doesn't work for testing, will pursue IoC

--- libs/github/src/lib/github.spec.ts
+++ libs/github/src/lib/github.spec.ts
@@ -1,7 +1,13 @@
-import { github } from './github';
+import axios from 'axios';
+import { getOpenPullRequestsWithCommitCount } from './github';
 
+jest.mock('axios');
 describe('github', () => {
-  it('should work', () => {
-    expect(github()).toEqual('github');
+  it('should work', async () => {
+    const prs = await getOpenPullRequestsWithCommitCount(
+      'expressjs',
+      'expressjs.com'
+    );
+    
   });
 });
